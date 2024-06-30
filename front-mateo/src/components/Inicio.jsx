47c2ea5c489c92d0391bc470b0bcc98d5b1b9905
Pablo <pf93520@gmail.com>
2024-06-30 13:41:24 -0300

    akdsfhALÑUHDFAPOUHEWPFOHEF

--- front-mateo/src/components/Inicio.jsx
+++ front-mateo/src/components/Inicio.jsx
@@ -7,28 +7,28 @@ function Inicio() {
       className="mt-5 p-5 rounded bg-dark text-white"
       style={{ backgroundColor: "lightgreen" }}
     >
-      <h1 style={{ textAlign: 'center' }}>Centro Deportivo</h1>
-      <p style={{ textAlign: 'center' }}>
+      <h1 style={{ textAlign: "center" }}>Centro Deportivo</h1>
+      <p>
         Explora el deporte más concurrido y practicado del país con
-        esta nueva página. Todo lo que necesitás saber sobre futbol 
+        esta nueva página. Todo lo que necesitás saber sobre futbol
         lo vas a encontrar aca.
       </p>
-      <p style={{ textAlign: 'center' }}>
-        Vas a tener al alcance de tu mano toda la información de 
+      <p>
+        Vas a tener al alcance de tu mano toda la información de
         la liga de futbol más competitiva de Sudamérica.
       </p>
-      <p style={{ textAlign: 'center' }}>¡Aprende, y sumate a este gran deporte!</p>
-      <Link to="/ligas" className="btn btn-primary">
-        <i className="fa fa-search"></i> Ligas Disponibles
-      </Link>
-      <Link to="/ligas" className="btn btn-primary">
-        <i className="fa fa-search"></i> Ligas Disponibles
-      </Link>
-      <Link to="/ligas" className="btn btn-primary">
-        <i className="fa fa-search"></i> Ligas Disponibles
-      </Link>
-      <span> </span>
-      <span></span>
+      <p>¡Aprende, y sumate a este gran deporte!</p>
+      <div style={{ textAlign: "center" }}>
+        <Link to="/ligas" className="btn btn-primary">
+          <i className="fa fa-search"></i> Ligas Disponibles
+        </Link>
+        <Link to="/ligas" className="btn btn-primary">
+          <i className="fa fa-search"></i> Jugadores Disponibles
+        </Link>
+        <Link to="/ligas" className="btn btn-primary">
+          <i className="fa fa-search"></i> Estadios Disponibles
+        </Link>
+      </div>
     </div>
   );
 }
---
+++ front-mateo/src/services/audio.service.js
@@ -0,0 +1,22 @@
+// services.js
+export default function useAudio(audioFilePath) {
+  const [playing, setPlaying] = useState(false);
+  const audioRef = useRef(null);
+
+  const playAudio = () => {
+    audioRef.current.play();
+    setPlaying(true);
+  };
+
+  const pauseAudio = () => {
+    audioRef.current.pause();
+    setPlaying(false);
+  };
+
+  useEffect(() => {
+    // Cleanup function to prevent memory leaks
+    return () => audioRef.current?.pause();
+  }, []);
+
+  return { playing, playAudio, pauseAudio };
+}
